ee814a5e647f92f8b13fc1195d24692659accf3f
Pablo Company <p.c.ramirez5150@gmail.com>
2024-02-07 20:19:48 +0100

    feat: Implements logout #7

---
+++ src/Guexit.IdentityProvider.Persistence/PersistedGrantMigrations/20240207171001_MigratesToIdentityServer7.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Guexit.IdentityProvider.Persistence.PersistedGrantMigrations
+{
+    /// <inheritdoc />
+    public partial class MigratesToIdentityServer7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "OperationalStore",
+                table: "ServerSideSessions",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateTable(
+                name: "PushedAuthorizationRequests",
+                schema: "OperationalStore",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReferenceValueHash = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    ExpiresAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Parameters = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PushedAuthorizationRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PushedAuthorizationRequests_ExpiresAtUtc",
+                schema: "OperationalStore",
+                table: "PushedAuthorizationRequests",
+                column: "ExpiresAtUtc");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PushedAuthorizationRequests_ReferenceValueHash",
+                schema: "OperationalStore",
+                table: "PushedAuthorizationRequests",
+                column: "ReferenceValueHash",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PushedAuthorizationRequests",
+                schema: "OperationalStore");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                schema: "OperationalStore",
+                table: "ServerSideSessions",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
--- src/Guexit.IdentityProvider.WebApi/Pages/Account/Logout/Index.cshtml.cs
+++ src/Guexit.IdentityProvider.WebApi/Pages/Account/Logout/Index.cshtml.cs
@@ -1,9 +1,11 @@
+using Duende.IdentityServer;
 using Duende.IdentityServer.Events;
 using Duende.IdentityServer.Extensions;
 using Duende.IdentityServer.Services;
 using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -58,36 +60,38 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnPost()
     {
-        if (User?.Identity.IsAuthenticated == true)
-        {
-            // if there's no current logout context, we need to create one
-            // this captures necessary info from the current logged in user
-            // this can still return null if there is no context needed
-            LogoutId ??= await _interaction.CreateLogoutContextAsync();
+        if (User?.Identity.IsAuthenticated != true)
+            return RedirectToPage("/Account/Logout/LoggedOut", new { logoutId = LogoutId });
+
+        // if there's no current logout context, we need to create one
+        // this captures necessary info from the current logged in user
+        // this can still return null if there is no context needed
+        LogoutId ??= await _interaction.CreateLogoutContextAsync();
                 
-            // delete local authentication cookie
-            await HttpContext.SignOutAsync();
+        // delete local authentication cookie
+        await HttpContext.SignOutAsync();
+        await HttpContext.SignOutAsync(IdentityServerConstants.SignoutScheme);
+        await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
 
-            // raise the logout event
-            await _events.RaiseAsync(new UserLogoutSuccessEvent(User.GetSubjectId(), User.GetDisplayName()));
+        // raise the logout event
+        await _events.RaiseAsync(new UserLogoutSuccessEvent(User.GetSubjectId(), User.GetDisplayName()));
 
-            // see if we need to trigger federated logout
-            var idp = User.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
+        // see if we need to trigger federated logout
+        var idp = User.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
 
-            // if it's a local login we can ignore this workflow
-            if (idp != null && idp != Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider)
+        // if it's a local login we can ignore this workflow
+        if (idp != null && idp != Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider)
+        {
+            // we need to see if the provider supports external logout
+            if (await HttpContext.GetSchemeSupportsSignOutAsync(idp))
             {
-                // we need to see if the provider supports external logout
-                if (await HttpContext.GetSchemeSupportsSignOutAsync(idp))
-                {
-                    // build a return URL so the upstream provider will redirect back
-                    // to us after the user has logged out. this allows us to then
-                    // complete our single sign-out processing.
-                    string url = Url.Page("/Account/Logout/Loggedout", new { logoutId = LogoutId });
+                // build a return URL so the upstream provider will redirect back
+                // to us after the user has logged out. this allows us to then
+                // complete our single sign-out processing.
+                string url = Url.Page("/Account/Logout/Loggedout", new { logoutId = LogoutId });
 
-                    // this triggers a redirect to the external provider for sign-out
-                    return SignOut(new AuthenticationProperties { RedirectUri = url }, idp);
-                }
+                // this triggers a redirect to the external provider for sign-out
+                return SignOut(new AuthenticationProperties { RedirectUri = url }, idp);
             }
         }
 
